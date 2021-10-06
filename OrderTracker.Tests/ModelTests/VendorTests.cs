1399c7df93341e88e184857a69262f0af72143e2
Kevin Funez <kevin.funez315@gmail.com>
2021-10-05 19:21:08 -0700

    add test for new Vendor constructor

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -2,3 +2,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using OrderTracker.Models;
 using System;
+
+namespace OrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor testVendor = new Vendor();
+      Assert.AreEqual(typeof(Vendor), testVendor.GetType());
+    }
+  }
+}
